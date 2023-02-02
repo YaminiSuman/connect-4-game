c5b0632e497a517d688ac469f92b8139d0dedce1
YaminiSuman <me.yamini.01@gmail.com>
2023-02-02 10:33:27 +0530

    setup server, schema and user model

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
+const uri =
+  "mongodb+srv://yamini:yamini-password@cluster0.bqsuu.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully!");
+});
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+const user = new User({ username: 'Yamini', password: '12345' });
+
+user.save((err) => {
+  if (err) throw err;
+  console.log("New user has been saved to db")
+})
+
 app.get("/", (req, res) => {
   res.send("Welcome to the game server!");
 });
